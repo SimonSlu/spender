adb0a45fabdc1e49bce31f32191e3e9c905d2025
SimonSlu <semenrus.qwe@gmail.com>
2025-07-01 21:30:06 +0400

    initial commit: basic Maven project structure

---
+++ src/main/java/com/github/SimonSlu/App.java
@@ -0,0 +1,13 @@
+package com.github.SimonSlu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
