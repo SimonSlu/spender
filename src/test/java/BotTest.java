4c07a5198538964664b8b56d8e9f772494f7f0d9
SimonSlu <semenrus.qwe@gmail.com>
2025-07-07 22:00:44 +0400

    Add test

---
+++ src/test/java/BotTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BotTest {
+    @Test
+    void testExample() {
+        assertEquals(2, 1 + 1);
+    }
+}
