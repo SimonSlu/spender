40d08c88268681eb4c7d05836c8262f431a60006
SimonSlu <semenrus.qwe@gmail.com>
2025-07-12 12:23:47 +0400

    Added config

--- src/main/java/com/github/SimonSlu/bot/SpenderBot.java
+++ src/main/java/com/github/SimonSlu/bot/SpenderBot.java
@@ -4,8 +4,33 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class SpenderBot extends TelegramLongPollingBot{
+    private final String botToken;
+
+    public SpenderBot(){
+        this.botToken = loadTokenFromConfig();
+    }
+
+    private String loadTokenFromConfig(){
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")){
+            if(input == null) {
+                throw new IllegalStateException("файл config.properties не найден");
+            }
+            Properties prop = new Properties();
+            prop.load(input);
+
+            String token = prop.getProperty("bot.token");
+            if(token == null || token.isEmpty()){
+                throw new IllegalStateException("Токен не найден в config.properties");
+            }
+            return token;
+        }catch (Exception e){
+            throw new RuntimeException("Ошибка загрузки токена", e);
+        }
+    }
 
     @Override
     public String getBotUsername() {
@@ -14,7 +39,7 @@ public class SpenderBot extends TelegramLongPollingBot{
 
     @Override
     public String getBotToken(){
-        return "7619002906:AAFm4TA2qNHsp5zOuUB7Q0_k6yFFUr3r0qc";
+        return botToken;
     }
 
     @Override
